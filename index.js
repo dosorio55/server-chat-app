d1a2e89cbd99fcac108ece56e15625d18e340d5d
dosorio55 <dosorio55@gmail.com>
2022-09-04 23:12:56 +0200

    chat app server first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import 'dotenv/config';
+
+const PORT = process.env.PORT;
+const router = express.Router();
+const app = express();
+
+// CORS
+app.use(cors({
+    origin: '*',
+    credentials: true,
+}));
+
+// MIDDLEWARES
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// MONGOOSE CONNECTION
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Successfully connected");
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
+
+//ROUTES
+router.get('/', (req, res) => res.send('sucessfully conected to Mongo'));
+
+//SERVER USE ROUTES
+app.use('/', router);
+
+//SERVER CONNECTION
+const server = app.listen(PORT, () => {
+    console.log(`chat running in http://localhost:${PORT}`)
+});
