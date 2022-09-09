b51855d9e95696e5859ac638feaa7d3f5d028d45
dosorio55 <dosorio55@gmail.com>
2022-09-09 22:14:15 +0200

    set avatar ready and working

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -2,31 +2,62 @@ import { Users } from "../models/user.model.js";
 import bcrypt from 'bcrypt';
 
 export const register = async (req, res, next) => {
-    
+
     const { userName, password } = req.body;
-    
-    
+
+
     try {
         const prevUserCheck = await Users.findOne({ username: userName });
-        
+
         if (prevUserCheck)
-        return res.json({ msg: "Username is already in use" });
-        
+            return res.json({ msg: "Username is already in use" });
+
         /* hash password */
         const hashedPassword = await bcrypt.hash(password, 10);
-        
-        const createdUser = await Users.create({
+
+        const user = await Users.create({
             username: userName,
             password: hashedPassword,
         });
-        delete createdUser.password;
-        return res.json({ status: 201, createdUser });
+        delete user.password;
+        return res.json({ status: 201, user, msg: 'registered' });
     } catch (error) {
         next(error);
     }
-    
+
 }
 
 export const login = async (req, res, next) => {
-    console.log(req.body);
+    try {
+        const { userName, password } = req.body;
+        const user = await Users.findOne({ userName });
+        if (!user)
+            return res.json({ msg: "Incorrect Username or Password" });
+
+        /* Comparare password */
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid)
+            return res.json({ msg: "Incorrect Username or Password" });
+        delete user.password;
+        return res.json({ user, msg: 'logged in!', status: 201 });
+    } catch (ex) {
+        next(ex);
+    }
+};
+
+export const setAvatar = async (req, res, next) => {
+    const userId = req.params.id;
+    const avatarImage = req.body.image;
+
+    try {
+        await Users.findByIdAndUpdate(userId, {
+            isAvatarImageSet: true, avatarImage
+        });
+        return res.json({
+            isSet: true,
+            image: avatarImage
+        })
+    } catch (error) {
+        next(error)
+    }
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { login, register } from "../controllers/user.controllers.js";
+import { login, register, setAvatar } from "../controllers/user.controllers.js";
 
 const userRoutes = express.Router()
 
 userRoutes.post("/register", register);
 userRoutes.post("/login", login);
+userRoutes.post("/setavatar/:id", setAvatar);
 /*router.get("/allusers/:id", getAllUsers);
-router.post("/setavatar/:id", setAvatar);
 router.get("/logout/:id", logOut); */
 
 export { userRoutes }
